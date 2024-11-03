08247081e72fa6ed45dac331360528f3399c3c6c
Abhshek-RanA <21f1006088@ds.study.iitm.ac.in>
2024-11-03 16:08:00 +0530

    about to git push a local branch

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 import flask_excel as excel
-from config import DevelopmentConfig
+from backend.config import DevelopmentConfig
+import os
+
+
+template_dir = os.path.abspath('frontend/templates')
+stactic_dir = os.path.abspath('frontend/static')
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder=template_dir, static_folder=stactic_dir)
     app.config.from_object(DevelopmentConfig)
     CORS(app)
     with app.app_context():
-        import frontend.application.controllers
-
+        import backend.application.controllers
     return app
 
 
