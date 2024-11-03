2eb2f9baaced4d682af1634f1c5f6b19dd38672e
Abhshek-RanA <21f1006088@ds.study.iitm.ac.in>
2024-11-03 23:15:39 +0530

    minor changes

---
+++ backend/test (2).py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
+size = len(basedir)
+maindir = basedir[:size-8]
+print(maindir)
+db_dir = os.path.join(basedir, "db_directory")
+print(db_dir)
