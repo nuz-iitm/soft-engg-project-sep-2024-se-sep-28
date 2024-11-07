f95a8671e07a3f9be7e48aa395f0b4023ef54a22
Abhshek-RanA <21f1006088@ds.study.iitm.ac.in>
2024-11-07 14:13:35 +0530

    working login and registration system by api.py

--- backend/application/data/models.py
+++ backend/application/data/models.py
@@ -7,7 +7,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String(255))
     fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
-    role = db.relationship('Role', secondary="roles_users", backref=db.backref('users', lazy='dynamic'))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary="roles_users", 
+                           backref=db.backref('users', lazy='dynamic'), 
+                           lazy='dynamic')
 
 
 class Role(db.Model, RoleMixin):
@@ -15,7 +18,9 @@ class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
-
+    # id = 1, admin
+    # id = 2, instructor
+    # id = 3, student
 
 class RolesUsers(db.Model):
     __tablename__ = 'roles_users'
