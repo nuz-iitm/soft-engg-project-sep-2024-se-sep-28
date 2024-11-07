90bb2e8c61fede832d088da93f15cf73bb7a5892
Abhshek-RanA <21f1006088@ds.study.iitm.ac.in>
2024-11-07 17:38:27 +0530

    logout for student working

--- frontend/static/components/side_bar_stdd.js
+++ frontend/static/components/side_bar_stdd.js
@@ -20,12 +20,35 @@ export default {
           Submission
         </router-link>
 
-        <router-link to="/" class="btn mt-4 w-100 text-center" 
-                   style=" background:none; border:2px red solid; font-size: 1.1rem; padding: 10px; font-weight: 500;">
-        Log out
-      </router-link>
+        <button  @click="logout" class="btn mt-4 w-100 text-center" 
+                    style=" background:none; border:2px red solid; font-size: 1.1rem; padding: 10px; font-weight: 500;">
+          Log out
+        </button>
       </div>
     `,
     name: 'side_bar_stdd',
-    methods: {}
+    methods: {
+      logout() {
+        const authToken = localStorage.getItem('auth-token');
+        localStorage.removeItem('auth-token');
+  
+        fetch('http://127.0.0.1:5000/api/logout', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${authToken}`
+          },
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('logout failed!');
+          }
+          return response.json();
+        })
+        .then(data =>{
+          alert('Logged out successfully');
+          this.$router.push("/");  // Redirect to the landing page
+        })
+      }
+    }
   };
