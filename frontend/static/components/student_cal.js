1e67b70a3453ae77aadeba67a9c004093fb1a94a
Abhshek-RanA <21f1006088@ds.study.iitm.ac.in>
2024-11-20 14:57:01 +0530

    calendar events student working

--- frontend/static/components/student_cal.js
+++ frontend/static/components/student_cal.js
@@ -1,48 +1,45 @@
 export default {
-    template: `
-        <div id="calendar"></div>
-    `,
-    name: 'student_cal',
+  template: `
+      <div id="calendar"></div>
+  `,
+  name: 'student_cal',
 
-    data() {
-      return {
-        events: [{
-          title: '',
-          start: ''
-        }]
-      }
-    },
-    mounted() {
-        this.initCalendar();
-      },
-    methods: {
-        initCalendar() {
-            const calendarEl = document.getElementById('calendar');
-            
-            const calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                eventClick: this.handleEventClick,
-                selectable: true,
-                events: () => this.getEvents(),
-              })
-              calendar.render()
-          },
-        handleEventClick(info){
-            alert(`Event: ${info.event.title}\nStart: ${info.event.start}`);
+  data() {
+    return {
+      events: []
+    }
+  },
+  mounted() {
+      this.initCalendar();
+  },
+  methods: {
+      initCalendar() {
+          const calendarEl = document.getElementById('calendar');
+          
+          const calendar = new FullCalendar.Calendar(calendarEl, {
+              initialView: 'dayGridMonth',
+              eventClick: this.handleEventClick,
+              selectable: false, // Disable event selection
+              events: () => this.getEvents(),
+            })
+            calendar.render()
         },
-        getEvents() {
-          return new Promise((resolve) => {
-              const authToken = localStorage.getItem('auth-token');
-              fetch('http://127.0.0.1:5000/api/student_events', {
-                  method: 'GET',
-                  headers: {
-                      'Content-Type': 'application/json',
-                      'Authorization': `Bearer ${authToken}`
-                  },
-              })
-              .then(response => response.json())
-              .then(data => resolve(data.map(event => ({ ...event }))));
-          });
-        }
+      handleEventClick(info){
+          alert(`Event: ${info.event.title}\nStart: ${info.event.start}`);
+      },
+      getEvents() {
+        return new Promise((resolve) => {
+            const authToken = localStorage.getItem('auth-token');
+            fetch('http://127.0.0.1:5000/api/student_events', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authToken}`
+                },
+            })
+            .then(response => response.json())
+            .then(data => resolve(data.map(event => ({ ...event }))));
+        });
       }
-  };
+    }
+};
